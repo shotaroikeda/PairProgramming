8ad9ba5d99a02bb1a5e3f18150ce3347eefcf70d
Keith <keith.a.tan@gmail.com>
2015-03-01 20:58:17 -0600

    Update RainGame.java

--- RainGame.java
+++ RainGame.java
@@ -1,4 +1,4 @@
-//UIUC CS125 FALL 2014 MP. File: RainGame.java, CS125 Project: PairProgramming, Version: 2015-02-23T22:06:27-0600.298194661
+//UIUC CS125 FALL 2014 MP. File: RainGame.java, CS125 Project: PairProgramming, Version: 2015-02-23T22:23:14-0600.443184943
 /**
  * @author replace-this-with-your-netids-on-this-line-here-with-a-comma-between-them
  */
@@ -12,11 +12,12 @@ public class RainGame {
 	
 		int x=0, y=0, dx=0, dy=0, score = 0;
 		String text = "";
+		int level = 0;
 		long startTime =System.currentTimeMillis();
-		int level = 0; //Initializes level
-		int levelcounter = 0; //Keeps count of numbers completed.
-		
+		int counter = 0;
+		int myscore = -100;
 		Zen.setFont("Helvetica-64");
+		
 		while (Zen.isRunning()) {
 
 			if (text.length() == 0) {
@@ -28,21 +29,24 @@ public class RainGame {
 				long elapsed = System.currentTimeMillis() - startTime;
 				startTime = System.currentTimeMillis();
 				score += 3000 / elapsed;
-				levelcounter++; // Adds to counter
-				if (levelcounter % 5 == 0) level++; //every 5 numbers the level goes up
+				counter++;
+				myscore = myscore + 100;
+				if (counter % 2 == 0) level++;
+				
 			}
 			
 			if (level < 10)Zen.setColor(0, 0, 0); // Color begins at BLACK
 			if (level >= 10 && level < 20)Zen.setColor(255, 0, 255); // Color at level 10 turns to PINK
-			if (level >= 20 && level < 30)Zen.setColor(0, 250, 0); // Color at level 20 goes to CRAZY GREEN
+			if (level >= 20 && level < 30)Zen.setColor(0, 250, 0); // Color at level 20 goes to WHITE
 			if (level >= 30)Zen.setColor((int) (Math.random() * 999), (int) (Math.random() * 999), (int) (Math.random() * 999)); // Color at level 30+ is RANDOM
+			
 			Zen.fillRect(0, 0, Zen.getZenWidth(), Zen.getZenHeight());
 
 			Zen.setColor(0, 255, 0);
 			Zen.drawText(text, x, y);
 			
 			Zen.drawText("Level: " + level,10,60);
-			Zen.drawText("Score: 0",10,130);
+			Zen.drawText("Score: " + score,10,130);
 			
 			x += dx;
 			y += dy;
@@ -58,9 +62,8 @@ public class RainGame {
 				if(c == text.charAt(0))
 					text = text.substring(1,text.length()); // all except first character
 			}
-			Zen.flipBuffer(); // Fixes the flicker
+			Zen.flipBuffer(); // stops the flicker
 			Zen.sleep(90);// sleep for 90 milliseconds
-
 		}
 	}
 
